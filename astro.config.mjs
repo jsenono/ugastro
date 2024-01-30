4149862ffea5a433d0f52efec60cc52d8bc2d053
jsenono <77863927+jsenono@users.noreply.github.com>
2024-01-30 18:12:04 +0200

    Updating changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
+// import relativeLinks from 'astro-relative-links';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  base: '/',
+ 
+  
+ 
 });
